c415ace3f7071785fad18388a6d7bab804ce6eb0
Raimon Esteve <raimonesteve@gmail.com>
2025-01-16 14:35:08 +0100

    FIX subdivision_types to set subdivision.id #173633

--- tests/test_module.py
+++ tests/test_module.py
@@ -20,23 +20,34 @@ class PartyZipTestCase(ModuleTestCase):
         Subdivision = pool.get('country.subdivision')
         PostalCode = pool.get('country.postal_code')
 
+        # allowed subdivisions by country code
+        # ('ES', ('autonomous city', 'province'))
+        # ('ID', ('autonomous province', 'province', 'special district', 'special region'))
+        # ('IT', ('province',))
         country1, country2 = Country.create([{
                     'name': 'Country 1',
+                    'code': 'ES',
                     }, {
                     'name': 'Country 2',
+                    'code': 'IT',
                     }])
-        subdivision1, subdivision2 = Subdivision.create([{
+        subdivision1, subdivision2, subdivision3 = Subdivision.create([{
                     'code': '1',
                     'name': 'Subdivision 1',
-                    'type': 'area',
+                    'type': 'province',
                     'country': country1.id,
                     }, {
                     'code': '2',
                     'name': 'Subdivision 2',
+                    'type': 'province',
+                    'country': country2.id,
+                    }, {
+                    'code': '3',
+                    'name': 'Subdivision 3',
                     'type': 'area',
                     'country': country2.id,
                     }])
-        postal_code1, postal_code2 = PostalCode.create([{
+        postal_code1, postal_code2, postal_code3 = PostalCode.create([{
                     'postal_code': 'postal_code1',
                     'city': 'city1',
                     'country': country1.id,
@@ -46,6 +57,11 @@ class PartyZipTestCase(ModuleTestCase):
                     'city': 'city2',
                     'country': country2.id,
                     'subdivision': subdivision2.id,
+                    }, {
+                    'postal_code': 'postal_code3',
+                    'city': 'city3',
+                    'country': country2.id,
+                    'subdivision': subdivision3.id,
                     }])
         party1, = Party.create([{
                     'name': 'Party 1',
@@ -85,5 +101,14 @@ class PartyZipTestCase(ModuleTestCase):
         self.assertEqual(address.country.id, country1.id)
         self.assertEqual(address.subdivision.id, subdivision1.id)
 
+        # not allow subdivision
+        Address.write([address], {
+                    'country': country2.id,
+                    'location': postal_code3.id,
+                    })
+        self.assertEqual(address.postal_code, 'postal_code3')
+        self.assertEqual(address.city, 'city3')
+        self.assertEqual(address.country.id, country2.id)
+        self.assertEqual(address.subdivision, None)
 
 del ModuleTestCase
